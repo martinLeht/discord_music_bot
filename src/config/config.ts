a89144e2fa0d672ea15a27f9bbda511e80b4a1bf
martinLeht <martin.lehtomaa@gmail.com>
2021-08-26 15:41:26 +0300

    Fix prod config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,15 @@
 import * as dotenv from 'dotenv';
 import { DotenvConfigOutput } from 'dotenv';
 
-let path = `${__dirname}/../../.env`;
-const result: DotenvConfigOutput = dotenv.config({ path: path });
- 
-if (result.error) {
-    throw result.error
+
+if (process.env.NODE_ENV === 'development') {
+    let path = `${__dirname}/../../.env`;
+    const result: DotenvConfigOutput = dotenv.config({ path: path });
+    if (result.error) {
+        throw result.error
+    }
 }
+
 export const PORT = process.env.PORT;
 export const TOKEN = process.env.TOKEN ? process.env.TOKEN : "";
 export const PREFIX = process.env.CMD_PREFIX ? process.env.CMD_PREFIX : "!" ;
